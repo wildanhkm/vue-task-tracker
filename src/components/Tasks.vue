e587f31e2ff14106c225166147621c10ef56bb66
wildanhkm <wildanhakim98@gmail.com>
2021-12-01 21:48:14 +0700

    add several components

---
+++ src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+export default {
+  name: "Tasks",
+  components:{
+      Task
+  }, 
+  props: {
+    tasks: Array,
+  },
+};
+</script>
