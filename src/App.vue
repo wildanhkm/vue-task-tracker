b02aaaa32f0681db943b89d6e9fa74c438e2af2b
wildanhkm <wildanhakim98@gmail.com>
2021-12-02 21:06:36 +0700

    add reminder and add task function

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,47 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks :tasks="tasks" />
+    <div v-if="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 export default {
   name: "App",
   components: {
     Header,
-    Tasks
+    Tasks,
+    AddTask,
   },
   data() {
     return {
       tasks: [],
+      showAddTask: false,
     };
+  },
+  methods: {
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+    deleteTask(id) {
+      if (confirm("Are you sure?"))
+        return (this.tasks = this.tasks.filter((task) => task.id !== id));
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
   },
   created() {
     this.tasks = [
@@ -35,7 +59,7 @@ export default {
       },
       {
         id: 3,
-        text: "Finish the project",
+        text: "Shopping",
         day: "April 8, 2021",
         reminder: false,
       },
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,10 @@
 <template>
   <div :key="task.id" v-for="task in tasks">
-    <Task :task="task" />
+    <Task
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task"
+    />
   </div>
 </template>
 
@@ -8,11 +12,12 @@
 import Task from "./Task";
 export default {
   name: "Tasks",
-  components:{
-      Task
-  }, 
+  components: {
+    Task,
+  },
   props: {
     tasks: Array,
   },
+  emits: ["delete-task", "toggle-reminder"],
 };
 </script>
