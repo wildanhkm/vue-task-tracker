a80617dcf5e8dc035abeb8c28be83f345cca0290
wildanhkm <wildanhakim98@gmail.com>
2021-12-03 22:06:29 +0700

    implementing vue router

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <p>Copyright Wildan Hakim &copy; 2021</p>
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+footer {
+  margin-top: 20px;
+  text-align: center;
+}
+a {
+  color: #333;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
+      v-show="homePage"
       @btn-click="$emit('toggle-add-task')"
       :text="showAddTask ? 'Close' : 'Add Task'"
       :color="showAddTask ? 'red' : 'green'"
@@ -19,6 +20,15 @@ export default {
   props: {
     title: String,
     showAddTask: Boolean,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
   },
 };
 </script>
